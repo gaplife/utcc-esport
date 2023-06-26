57ad50fc1ef22d2d7a44aa812927a86e713a7230
Maswnk <2110717102002@live4.utcc.ac.th>
2023-06-27 03:56:18 +0700

    Auto Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:utcc_esport/src/app.dart';
 
+Future<String?> getUserTypeFromFirestore(String uid) async {
+  DocumentSnapshot<Map<String, dynamic>> snapshot =
+      await FirebaseFirestore.instance.collection('Users').doc(uid).get();
+  if (snapshot.exists) {
+    Map<String, dynamic> data = snapshot.data()!;
+    String? userType = data['userType'];
+    return userType;
+  }
+  return null;
+}
+
 Future<Null> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp().then((value) async {
-    await FirebaseAuth.instance.authStateChanges().listen((event) {
-      if (event != null) {
-        initialRoute = '/orglauncher';
+    await FirebaseAuth.instance.authStateChanges().listen((User? user) async {
+      if (user != null) {
+        String? userType = await getUserTypeFromFirestore(user.uid);
+        if (userType == 'Player') {
+          initialRoute = '/launcher';
+          runApp(const App());
+        } else if (userType == 'Organizer') {
+          initialRoute = '/orglauncher';
+          runApp(const App());
+        } else {
+          runApp(const App());
+        }
+      } else {
+        runApp(const App());
       }
-      runApp(const App());
     });
   });
 }
--- lib/src/models/profile.dart
+++ lib/src/models/profile.dart
@@ -1,6 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:convert';
-
 class Profile {
   String? email;
   String? password;
@@ -17,80 +14,4 @@ class Profile {
     this.userType,
     this.profileImageUrl,
   });
-
-  Profile copyWith({
-    String? email,
-    String? password,
-    String? confirmpassword,
-    String? username,
-    String? userType,
-    String? profileImageUrl,
-  }) {
-    return Profile(
-      email: email ?? this.email,
-      password: password ?? this.password,
-      confirmpassword: confirmpassword ?? this.confirmpassword,
-      username: username ?? this.username,
-      userType: userType ?? this.userType,
-      profileImageUrl: profileImageUrl ?? this.profileImageUrl,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'email': email,
-      'password': password,
-      'confirmpassword': confirmpassword,
-      'username': username,
-      'userType': userType,
-      'profileImageUrl': profileImageUrl,
-    };
-  }
-
-  factory Profile.fromMap(Map<String, dynamic> map) {
-    return Profile(
-      email: map['email'] != null ? map['email'] as String : null,
-      password: map['password'] != null ? map['password'] as String : null,
-      confirmpassword: map['confirmpassword'] != null
-          ? map['confirmpassword'] as String
-          : null,
-      username: map['username'] != null ? map['username'] as String : null,
-      userType: map['userType'] != null ? map['userType'] as String : null,
-      profileImageUrl: map['profileImageUrl'] != null
-          ? map['profileImageUrl'] as String
-          : null,
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory Profile.fromJson(String source) =>
-      Profile.fromMap(json.decode(source) as Map<String, dynamic>);
-
-  @override
-  String toString() {
-    return 'Profile(email: $email, password: $password, confirmpassword: $confirmpassword, username: $username, userType: $userType, profileImageUrl: $profileImageUrl)';
-  }
-
-  @override
-  bool operator ==(covariant Profile other) {
-    if (identical(this, other)) return true;
-
-    return other.email == email &&
-        other.password == password &&
-        other.confirmpassword == confirmpassword &&
-        other.username == username &&
-        other.userType == userType &&
-        other.profileImageUrl == profileImageUrl;
-  }
-
-  @override
-  int get hashCode {
-    return email.hashCode ^
-        password.hashCode ^
-        confirmpassword.hashCode ^
-        username.hashCode ^
-        userType.hashCode ^
-        profileImageUrl.hashCode;
-  }
 }
